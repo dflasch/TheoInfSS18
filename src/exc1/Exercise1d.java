7ebe3d527a1bd4a4a0db2d273820a8b3ddcc759f
Daniel Flasch <mail@dflasch.de>
2018-03-22 18:09:03 +0100

    reads graph descriptions from textfiles

--- src/exc1/Exercise1d.java
+++ src/exc1/Exercise1d.java
@@ -8,23 +8,21 @@ import org.jgrapht.io.ExportException;
 import lib.Edge;
 import lib.Exporter;
 import lib.Parser;
+import lib.Reader;
 import lib.Vertex;
 
 public class Exercise1d {
 
-    final static String GRAPH_DESCRIPTION = "" + "# 9 Knotendefinitionen, A bis I\n" + "knoten A 1\n" + "knoten B 2 \n"
-            + "knoten C 3\n" + "knoten D 4\n" + "knoten E 5\n" + "knoten F 6\n" + "knoten G 7\n" + "knoten H 8\n"
-            + "knoten I 9\n" + "\n" + "# Kantendefinitionen \n" + "kante A B 2\n" + "kante A F 9\n" + "kante A G 15\n"
-            + "kante B C 4\n" + "kante B G 6\n" + "kante C D 2\n" + "kante C I 15\n" + "kante D E 1\n" + "kante D I 1\n"
-            + "kante E F 6\n" + "kante E H 3\n" + "kante F H 11\n" + "kante G H 15\n" + "kante G I 2\n" + "kante H I 4";
-
     public static void main(String[] args) throws ExportException, IOException {
+        Reader fileReader = new Reader();
         Parser graphParser = new Parser();
-
-        DefaultDirectedWeightedGraph<Vertex, Edge> graph = graphParser.createDirectedWeightedGraph(GRAPH_DESCRIPTION);
+        
+        String graphDescription = fileReader.readFile("input/Dijkstra.txt");
+        
+        DefaultDirectedWeightedGraph<Vertex, Edge> graph = graphParser.createDirectedWeightedGraph(graphDescription);
 
         new Exporter(graph).printToConsole();
-        new Exporter(graph).printToFile("graphs/exercise-1-d.dot");
+        new Exporter(graph).printToFile("output/exercise-1-d.dot");
     }
 
 }
---
+++ src/lib/Reader.java
@@ -0,0 +1,27 @@
+package lib;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+    
+    public String readFile(String path) throws IOException {
+        String eachLine = "";
+        File file = new File(path);
+        String readText = "";
+        
+        if (!file.exists()) return readText;
+        
+        BufferedReader fileReader = new BufferedReader(new FileReader(file));
+        
+        while((eachLine = fileReader.readLine())!=null) 
+            readText += eachLine.trim() + "\n";
+        
+        fileReader.close();
+        
+        return readText;
+    }
+    
+}
